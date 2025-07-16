a643451472c3ee1d76d244bf9c0538e19965afd4
PALAK BISHT <bishtpalak16@gmail.com>
2025-07-16 10:08:44 +0530

    updates

--- ternaryOp.java
+++ ternaryOp.java
@@ -7,8 +7,19 @@ public class ternaryOp {
         int a = sc.nextInt();
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+        System.out.print("Enter fourth number: ");
+        int d = sc.nextInt();
+        
+        ////////for 2 numbers the ternanry oepration would be;
+        // int max = (a > b) ? a : b;
 
-        int max = (a > b) ? a : b;
+        ////////for 3 numbers the ternary operation would be;
+        // int max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+
+        //////// for 4 numbers the ternary operation would be;
+        int max = (a > b) ? (a > c ? (a > d ? a : d) : (c > d ? c : d)) : (b > c ? (b > d ? b : d) : (c > d ? c : d));
 
         System.out.println("Largest number is: " + max);
     }
